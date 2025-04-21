863d63edde5ed4c91aa30ad8a48f52aa4f0ca4de
Dmitry Patin <multik.patin@yandex.ru>
2025-04-22 00:00:08 +0300

    fix GetAll orders

--- internal/adapters/database/postgres/repositories/order.go
+++ internal/adapters/database/postgres/repositories/order.go
@@ -119,10 +119,6 @@ func (r *OrdersRepository) GetAll(ctx context.Context, statuses []enums.OrderSta
 				"status", status,
 			)
 		} else {
-			r.logger.Infow(
-				"GetAll",
-				"OrderDB", w,
-			)
 			orders = append(orders, &w)
 		}
 
@@ -147,32 +143,16 @@ func (r *OrdersRepository) BatchUpdate(ctx context.Context, orders []*dtos.Updat
 
 	for _, order := range orders {
 		status, err := enums.MutateLoyaltyToOrderStatus(order.Status)
-		if err == nil {
-			r.logger.Infow(
-				"BatchUpdate",
-				"order.Accrual", order.Accrual,
-				"status", status,
-				"order.I", order.ID,
-			)
-			_, err := r.db.Connection.ExecContext(ctx, query, order.Accrual, status, order.ID)
-			if err != nil {
-				tx.Rollback()
-				return err
-			}
+		if err != nil {
+			return err
+		}
+		_, err = tx.ExecContext(ctx, query, order.Accrual, status, order.ID)
+		if err != nil {
+			tx.Rollback()
+			return err
 		}
 	}
 
 	tx.Commit()
-
-	var statusesTest []enums.OrderStatusEnum
-
-	data, err := r.GetAll(ctx, statusesTest)
-	for _, d := range data {
-		r.logger.Infow(
-			"BatchUpdateGetAll",
-			"data", d,
-		)
-	}
-
 	return nil
 }
